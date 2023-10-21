593fa550855c55c106a2a87e3cfe2b04fe853efc
JhonatanLucena <jhonatan.jhon9@hotmail.com>
2023-10-21 00:13:01 -0400

    criaçao da pagina header e começo do form que aidna esta na pagina index

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet">
+<link href="js/bootstrap.js" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-info ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Inicio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav ">
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link "  href="#">Listar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inserir Pessoas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastrar Carros</a>
+          <!--Cadastro de carros-->
+        </li>
+        <li class="nav-item">
+          <a class="nav-link"  href="#">Modificar</a>
+          <!--modficar seria a parte de editar, e excluir no banco de dados-->
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php
+include_once 'header.php';
+?>
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,24 +11,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
   </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  testando
-</form>
+  <body >
+    
+      <form class = "container-md">
+        <div class="md-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control form-control-lg" aria-describedby="emailHelp">
+        </div>
+        <div class="md-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control form-control-lg">
+        </div>
+        <div class="md-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-info">Submit</button>
+    </form>
+  
   </body>
 </html>
